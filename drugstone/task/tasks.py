3c32bd3fd8729616a2b7bf2cff94234d8ad855af
Ugur Turhan <ugur.turhan@uni-hamburg.de>
2022-03-01 14:10:33 +0100

    added union and intersection for tasks

--- drugstone/task/tasks.py
+++ drugstone/task/tasks.py
@@ -35,22 +35,26 @@ class Tasks:
     def get_intersection(self) -> TaskResult:
         drugs = {}
         genes = {}
-        r_drugs = []
-        r_genes = []
+        first = True
         for t in self.__tasks:
             r = t.get_result()
-            r_drugs.append(r.get_drugs())
-            r_genes.append(r.get_genes())
+            if first:
+                drugs = r.get_drugs()
+                genes = r.get_genes()
+                first = False
+            else:
+                drugs = {d: drugs[d] for d in drugs if d in r.get_drugs()}
+                genes_intersection = {}
+                for gene, detail in r.get_genes().items():
+                    if gene in genes:
+                        genes_intersection = {**genes_intersection, gene: detail}
+                        edges_a = genes[gene]["has_edges_to"]
+                        edges_b = detail["has_edges_to"]
+                        new_edges = list(set(edges_a) & set(edges_b))
+                        genes_intersection[gene]["has_edges_to"] = new_edges
+                genes = genes_intersection.copy()
+        for d in drugs:
+            drugs[d].pop("score")
+        for g in genes:
+            genes[g].pop("score")
         return TaskResult(drugs=drugs, genes=genes)
-
-    # if self.__is_wrapper:
-    #     if self.__tasks:
-    #         drugs = {}
-    #         genes = {}
-    #         for t in self.__tasks:
-    #             r = t.get_result()
-    #             drugs = {**drugs, **r.get_drugs()}
-    #             genes = {**genes, **r.get_genes()}
-    #         return TaskResult(drugs=drugs, genes=genes)
-    #     else:
-    #         return TaskResult()
